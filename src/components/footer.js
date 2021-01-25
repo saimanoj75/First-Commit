a519550ea832d02a126f76f81277666bda8e01d0
Saimanoj <36559913+saimanoj75@users.noreply.github.com>
2021-01-25 17:06:11 +0530

    first commit

---
+++ src/components/footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const Footer = ({ author, profile }) => (
+    <footer>
+        Made with 
+        
+        <i className="em em-heart" style={{width: '15px', height: '15px'}}></i>
+        <a href="https://github.com/saimanoj75"> @saimanoj75</a>
+    </footer>
+)
+
+export default Footer
